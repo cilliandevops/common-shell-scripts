a2782d3634704068eca807e3812f08915618ee3f
cilliandevops <cilliandevops@gmail.com>
2023-08-23 10:36:14 +0800

    shellscripts-1

---
+++ The Linux system sends an alarm script.sh
@@ -0,0 +1,6 @@
+# The Linux system sends an alarm script
+# yum install mailx
+# vi /etc/mail.rc  
+set from=baojingtongzhi@163.com smtp=smtp.163.com
+set smtp-auth-user=baojingtongzhi@163.com smtp-auth-password=123456
+set smtp-auth=login
